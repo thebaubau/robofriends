be64acaeb9ae6d0aa375323de83b01687d0bf7d1
Daniel <fintinariu.daniel@gmail.com>
2022-02-12 23:38:25 +0100

    Initial commit

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Hello extends Component {
+    render() {
+        
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <p>Welcome to React</p>
+            </div>
+            
+        );
+    }
+}
+
+export default Hello
