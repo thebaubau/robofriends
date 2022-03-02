11a4c02d1008b7e03a89e7d3c4d02ee909e24891
Daniel <fintinariu.daniel@gmail.com>
2022-03-02 17:00:19 +0100

    Redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provide, connect } from 'react-redux'
+import { Provider, connect } from 'react-redux'
 import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App'
 import reportWebVitals from './reportWebVitals';
+import { searchRobots } from './reducers';
 import 'tachyons';
+
+const store = createStore(searchRobots)
 
 ReactDOM.render(
   <React.StrictMode>
